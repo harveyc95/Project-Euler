071ebd66b2e8012acfc4bf0edca5af9369ee8698
Harvey Chen <harveyc95@gmail.com>
2015-10-08 23:39:10 -0400

    fixed numbering

---
+++ 007.cpp
@@ -0,0 +1,45 @@
+/* Project Euler Problem 7 - https://projecteuler.net/problem=7
+ * 10001st prime
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * What is the 10 001st prime number?
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+bool isPrime (int n);
+
+int main () {
+	bool notFound = true;
+	int i = 2;
+	int key = 10001;
+	int counter = 0;
+	while (true) {
+		if (isPrime(i)) {
+			// printf("prime: %d\n", i);
+			counter++;
+		}
+		if (counter >= key) {
+			break;
+		}
+		i++;
+	}
+	printf("%d\n", i);
+}
+
+bool isPrime (int n) {
+	if (n <= 1)
+		return false;
+	if (n == 2)
+		return true;
+	if (n%2 == 0)
+		return false;
+	int m = sqrt(n);
+
+	for (int i = 3; i <= m; i++) {
+		if (n%i == 0)
+			return false;
+	}
+
+	return true;
+}
---
+++ 009.cpp
@@ -0,0 +1,25 @@
+/* Project Euler Problem 9 - https://projecteuler.net/problem=9
+ * Special Pythagorean triplet
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	for (int A = 1; A < 999; A++) {
+		for (int B = 1; (A + B < 1000 && B < 999); B++) {
+			int C = 1000 - A - B;
+			if ((A*A + B*B) == (C*C)) {
+				printf("Found a triple!\n");
+				printf("A: %d, B: %d, C:%d\n", A, B, C);
+				printf("Product: %d\n", A*B*C);
+				return 0;
+			}
+		}
+	}
+}
