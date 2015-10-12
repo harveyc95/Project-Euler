3a742f3b53422845c89d7cda73dd3815c6b43a6f
Harvey Chen <harveyc95@gmail.com>
2015-10-12 12:19:22 -0400

    Added problem 6 for python

---
+++ 006.py
@@ -0,0 +1,19 @@
+''' Project Euler Problem 6 - https://projecteuler.net/problem=6
+ * Sum square difference
+ * The sum of the squares of the first ten natural numbers is,
+ * 1^2 + 2^2 + ... + 10^2 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)^2 = 55^2 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * 
+ * Created by Harvey Chen on 12/10/2015.
+ * Copyright (c) 2015 Harvey Chen. All rights reserved.
+'''
+
+n = 100
+sumOfSquare = n*(n+1)*(2*n+1)/6
+squareOfSum = (n*(n+1)/2) * (n*(n+1)/2)
+print(str(int(squareOfSum - sumOfSquare)))
+
+#Ans = 25164150
