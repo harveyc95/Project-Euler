7a9f1134c5b6ed516cae73f685f2ee34fb4d8a6d
Harvey Chen <harveyc95@gmail.com>
2015-11-01 17:33:30 -0500

    Added template.cpp - includes useful macros and all required libraries

---
+++ Useful Fuctions/template.cpp
@@ -0,0 +1,49 @@
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include <algorithm>
+# include <numeric>
+# include <cstdio>
+# include <cmath>
+# include <cstdlib>
+# include <cstring>
+# include <vector>
+# include <list>
+# include <set>
+# include <map>
+# include <stack>
+# include <queue>
+# include <cctype>
+# include <climits>
+
+using namespace std;
+ 
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+
+#define OR |
+#define AND &
+#define XOR ^
+
+// Input macros
+#define s(n)	scanf("%d",&n)
+#define sc(n)	scanf("%c",&n)
+#define sl(n)   scanf("%lld",&n)
+#define sf(n)   scanf("%lf",&n)
+#define ss(n)   scanf("%s",n)
+
+// Useful constants
+#define INF	(int)1e9
+#define EPS	1e-9
+#define PI (double)(3.141592653589793)
+
+// Some common useful functions
+#define maX(a,b)	((a) > (b) ? (a) : (b))
+#define miN(a,b)	((a) < (b) ? (a) : (b))
+
+int main (int argc, char *argv[]) {
+
+}
