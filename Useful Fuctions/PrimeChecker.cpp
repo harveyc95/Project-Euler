aa47f7c20baaebf18c905cca1c98f312d07ded2d
Harvey Chen <harveyc95@gmail.com>
2015-10-31 20:16:40 -0400

    Updated PrimeChecker.cpp

--- Useful Fuctions/PrimeChecker.cpp
+++ Useful Fuctions/PrimeChecker.cpp
@@ -5,18 +5,13 @@
 // Copyright (c) 2015 Harvey Chen. All rights reserved.
 
 bool isPrime (int n) {
-	if (n <= 1)
-		return false;
-	if (n == 2)
-		return true;
-	if (n%2 == 0)
-		return false;
+	if (n <= 1)	return false;
+	if (n == 2)	return true;
+	if (n%2 == 0)	return false;
 	int m = sqrt(n);
 
 	for (int i = 3; i <= m; i++) {
-		if (n%i == 0)
-			return false;
+		if (n%i == 0)	return false;
 	}
-
 	return true;
 }
